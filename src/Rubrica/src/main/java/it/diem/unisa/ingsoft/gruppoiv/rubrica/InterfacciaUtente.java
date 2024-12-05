7a00a986eb33e1dfa35278839814137e2f8fd856
Emanuel  Chirico <emanuelchirico03@gmail.com>
2024-12-05 20:14:56 +0100

    Commit n°1 : aggiunte classi: - Contatto.java - GestioneFile.java - InterfacciaUtente.java - Rubrica,java

---
+++ src/Rubrica/src/main/java/it/diem/unisa/ingsoft/gruppoiv/rubrica/InterfacciaUtente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.diem.unisa.ingsoft.gruppoiv.rubrica;
+
+public class InterfacciaUtente {
+
+    private Rubrica rubrica;
+
+    private GestioneFile gestioneFile;
+
+    public void start() {
+    }
+
+    public void main() {
+    }
+
+    public void visualizzaRubrica() {
+    }
+
+    public void visualizzaRicerca() {
+    }
+    
+    public void init(){}
+    
+    public void stop(){}
+    
+}
+
---
+++ src/Rubrica/src/main/java/it/diem/unisa/ingsoft/gruppoiv/rubrica/Rubrica.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.diem.unisa.ingsoft.gruppoiv.rubrica;
+
+import java.util.List;
+
+public class Rubrica {
+
+    private List<Contatto> contatti;
+
+    public void aggiungiContatto() {
+    }
+
+    public void rimuoviContatto() {
+    }
+
+    public void modificaContatto() {
+    }
+
+    public void ricercaContatto() {
+    }
+    
+    public void salvaModifiche(){
+    }
+}
